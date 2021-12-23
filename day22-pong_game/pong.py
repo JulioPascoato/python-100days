4da8ffa1fcc27256f31071720e29b1f44c58b24f
Julio Pascoato <jpascoato@gmail.com>
2021-12-23 18:58:16 -0300

    Day 22 - Intermediate - Pong Arcade game

---
+++ day22-pong_game/pong.py
@@ -0,0 +1,61 @@
+from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import  Scoreboard
+
+import time
+
+WIDTH = 800
+HEIGHT = 600
+BG_COLOR = "black"
+
+screen = Screen()
+screen.title("Pong - The Arcade Game")
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.bgcolor(BG_COLOR)
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.05)
+    screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+screen.exitonclick()
